0d59f88578fa4a9e341a816e605e0d8b229dbc18
sahul-yadav <yadavsahul220@gmail.com>
2025-12-12 13:15:09 +0530

    Add initial routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route } from 'react-router'
+import { Routes } from 'react-router'
+import Layout from './Layout'
+import Home from './pages/Home'
+import UserProfile from './pages/UserProfile'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='text-8xl'>
-      sahul
-    </h1>
+   
+    <Routes>
+      <Route element={<Layout/>}>
+  <Route path='/' element={<Home/>}/> 
+      <Route path='/user' element={<UserProfile/>}/>
+       <Route path='/product' element={<ProductDetails/>}/>
+        <Route path='/ cart' element={<Cart/>}/>
+      
+        
+      </Route>
+    </Routes>
     </>
   )
 }
